5d92d3c588b6f0b43714d8620331cc7ef250754a
Philip Ford <paf1687@gmail.com>
2015-09-04 00:34:58 -0400

    Obtained a Twitter access token, added the Twitter secret, key, access token, and secret token to a local properties file, and updated Twitter4j.  I received a 401 until updated to Twitter4j 4.0.4

--- src/main/scala/com/macedon/scala/examples/twitter/ScalaTwitterClientExample.scala
+++ src/main/scala/com/macedon/scala/examples/twitter/ScalaTwitterClientExample.scala
@@ -2,30 +2,52 @@ package com.macedon.scala.examples.twitter
 import twitter4j.TwitterFactory
 import twitter4j.Twitter
 import twitter4j.conf.ConfigurationBuilder
- 
+import java.util.Properties
+import java.io.FileInputStream
+
+
 object ScalaTwitterClientExample {
    
   def main(args : Array[String]) {
-     
+
+    val (key, secret, token, secretToken) =
+      try {
+        val prop = new Properties()
+        prop.load(ScalaTwitterClientExample.getClass.getClassLoader.getResourceAsStream("auth.properties"))
+
+        (
+          prop.getProperty("consumer.key"),
+          prop.getProperty("consumer.secret"),
+          prop.getProperty("access.token"),
+          prop.getProperty("access.token.secret")
+          )
+      } catch { case e: Exception =>
+        e.printStackTrace()
+        sys.exit(1)
+      }
+
+
     // (1) config work to create a twitter object
     val cb = new ConfigurationBuilder()
     cb.setDebugEnabled(true)
-      .setOAuthConsumerKey("YOUR KEY HERE")
-      .setOAuthConsumerSecret("YOUR SECRET HERE")
-      .setOAuthAccessToken("YOUR ACCESS TOKEN")
-      .setOAuthAccessTokenSecret("YOUR ACCESS TOKEN SECRET")
+      .setOAuthConsumerKey(key)
+      .setOAuthConsumerSecret(secret)
+      .setOAuthAccessToken(token)
+      .setOAuthAccessTokenSecret(secretToken)
     val tf = new TwitterFactory(cb.build())
     val twitter = tf.getInstance()
+
+    println(s"Twitter:  $twitter")
  
     // (2) use the twitter object to get your friend's timeline
-    val statuses = twitter.getFriendsTimeline()
+    val statuses = twitter.getHomeTimeline
     System.out.println("Showing friends timeline.")
     val it = statuses.iterator()
-    while (it.hasNext()) {
+    while (it.hasNext) {
       val status = it.next()
-      println(status.getUser().getName() + ":" +
-              status.getText());
+      println(status.getUser.getName + ":" + status.getText)
     }
- 
+
   }
+
 }
